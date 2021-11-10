09963f333b7526553ad481861c341c1f7739ed64
Kacper Maj <60718448+kapisolec@users.noreply.github.com>
2021-11-11 00:33:32 +0100

    project setup

---
+++ src/Components/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Bsc from '../BscBot/Bsc';
+import Header from '../Header/Header';
+
+function App() {
+    return (
+        <div className="App">
+            <Header />
+            <Bsc />
+        </div>
+    );
+}
+
+export default App;
