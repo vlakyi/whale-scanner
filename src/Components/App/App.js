861bf01d7e2c00ccc43598603983cf82e00fb1c7
Kacper Maj <60718448+kapisolec@users.noreply.github.com>
2021-11-11 09:29:15 +0100

    deleted redundant elements

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-// import Bsc from '../BscBot/Bsc';
 import Header from '../Header/Header';
 
 function App() {
     return (
         <div className="App">
             <Header />
-            {/* <Bsc /> */}
         </div>
     );
 }
