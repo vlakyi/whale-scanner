4bf5eb50093cd38bef043b7be3edaf568ab82698
Kacper Maj <60718448+kapisolec@users.noreply.github.com>
2021-11-13 17:21:11 +0100

    Defined project structure

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,3 @@
-import './Header.css'
-
 const Header = () => {
   const headerHandler = (e) => {
     e.preventDefault()
---
+++ src/Components/Scanner.js
@@ -0,0 +1,10 @@
+const Scanner = (props) => {
+  props ? props : {}
+  return (
+    <div>
+      <div>Address Scanner</div>
+      <div>Test</div>
+    </div>
+  )
+}
+export default Scanner
